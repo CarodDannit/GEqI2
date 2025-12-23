96cd37422de64de69c6369c9e4d3caf64f46583a
Arif <dopymonster@gamil.com>
2025-12-23 23:22:44 +0800

    kjty

--- GEqI/Main.cpp
+++ GEqI/Main.cpp
@@ -8,15 +8,7 @@ using namespace std;
 #include "Consumable.h"
 #include "Maintenance.h"
 
-//hellobhjyuvt687yu
-//ma arif
-//a
-//
-//imran
-// yo
-// LOREM
-//HI ARIF
-//HI GUys
+//AHI AHMAD
 
 int main() {
 	cout << "\033[31m||===== GEqI - Gym Equipment Inventory System =====||\033[0m\n";

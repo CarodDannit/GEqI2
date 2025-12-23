1a58dc80f9cd361219484269794dfab76f3904d8
Chernobylian-Crow <madlycrow@gmail.com>
2025-12-23 23:56:36 +0800

    added SampleData.h, incomplete

---
+++ GEqI/SampleData.h
@@ -0,0 +1,13 @@
+#ifndef SAMPLEDATA_H
+#define SAMPLEDATA_H
+#include "List.h"
+#include "Item.h"
+#include "Consumable.h"
+#include "Equipment.h"
+
+void insertData(List<Equipment> equipments, List<Consumable> consumables)
+{
+
+}
+
+#endif
